d9a658bb8f676bcc00c5497cb008412c33b68647
Steve <frombs@rpi.edu>
2021-01-31 17:35:06 -0500

    Final push

---
+++ Assets/Scripts/Ender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ender : MonoBehaviour
+{
+    public ScoreTracker myScore;
+    int score;
+    public Text textScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = myScore.GetScore();
+        textScore.text = "Total Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
